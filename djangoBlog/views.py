c08f27833af9e72cce66b6e2008f1cbb9fabf88e
misterpentest <xamirranjbar@gmail.com>
2021-02-03 19:09:19 +0330

    create project

---
+++ djangoBlog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
+def about(request):
+    # return HttpResponse('hi there! Im hello world')
+    return render(request , 'about.html')
+def home(request):
+    # return HttpResponse('WELCOME TO HOME')
+    return render(request , 'Home.html')
